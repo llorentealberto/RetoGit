584e7e624d6255022c7d72dce1ef01d236bce28c
llorentealberto <[llorentealberto@hotmal.com]>
2024-03-05 15:59:35 +0100

    cambios hechos

---
+++ calculadora.js
@@ -0,0 +1,27 @@
+function suma(op1,op2){
+
+    console.log(op1+op2);
+}
+
+function resta(op1,op2){
+    console.log(op1-op2);
+}
+
+function multiplication(op1,op2){
+
+    console.log(op1*op2);
+}
+
+function division(op1,op2){
+
+    console.log(op1/op2);
+}
+
+// añado una funcion que haga el cuadrado de un numero 
+function cuadrado(op1){
+
+    console.log(op1*op1);
+
+
+
+}
